6421268fb3a41017db992c8e61f6984bce489dc5
nygellopez <nygel.lopez@wishabi.com>
2018-02-10 05:55:10 -0500

    removed introduction animation

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function animatePage() {
-	$(".animate").each(function(i) {
-        $(this).delay(i * 1000).hide().fadeIn(1000); 
-    });
-};
-
-$(function() {
-	$(document).ready(animatePage());
-});
